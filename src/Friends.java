bd0ad05a66598664e5ef373f25065c653cc60f7a
abalmasDA <abalmasit@gmail.com>
2023-06-07 16:27:43 +0300

    Commit1

--- src/Friends.java
+++ src/Friends.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Friends {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Friends {
+    private ArrayList<String> listOfFriends = new ArrayList<>();
+
+    public void addingElementToList(String nameFriend) {
+        listOfFriends.add(listOfFriends.size(), nameFriend);
+    }
+
+
+    public void printList() {
+        System.out.println(listOfFriends);
+
+    }
+
+
+    public void sortListByAlphabeticalOrder() {
+        Collections.sort(listOfFriends);
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+/**
+ * Використовуючи Intelij IDEA, створити клас Friends.
+ * За допомогою методів ArrayList додати масив імена друзів. Вивести список друзів, після вивести список друзів,
+ * відсортувавши в алфавітному порядку.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Friends friends = new Friends();
+        friends.addingElementToList("Dmytro");
+        friends.addingElementToList("Liza");
+        friends.addingElementToList("Alex");
+        friends.addingElementToList("Ivan");
+        friends.addingElementToList("Victoria");
+        friends.printList();
+        friends.sortListByAlphabeticalOrder();
+        friends.printList();
+
+    }
 }
